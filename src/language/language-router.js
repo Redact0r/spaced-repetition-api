b811be2563b013701c9b10662504b4a9d70e46c9
Tyler Sharp <johntylersharp@gmail.com>
2020-06-24 14:31:22 -0400

    Implemented head, trying to implement /guess

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const LanguageService = require("./language-service");
 const { requireAuth } = require("../middleware/jwt-auth");
+const parser = express.json();
 
 const languageRouter = express.Router();
 
@@ -41,16 +42,69 @@ languageRouter.get("/", async (req, res, next) => {
 });
 
 languageRouter.get("/head", async (req, res, next) => {
-  const [nextWord] = await LanguageService.getLanguageWords(
-    req.app.get("db"),
-    req.language.id
-  );
-  res.json({ nextWord: nextWord.original });
+  try {
+    const [nextWord] = await LanguageService.getLanguageWords(
+      req.app.get("db"),
+      req.language.id
+    );
+    res.json({
+      nextWord: nextWord.original,
+      totalScore: req.language.total_score,
+      wordCorrectCount: nextWord.correct_count,
+      wordIncorrectCount: nextWord.incorrect_count,
+    });
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
-languageRouter.post("/guess", async (req, res, next) => {
-  // implement me
-  res.send("implement me!");
+languageRouter.post("/guess", parser, async (req, res, next) => {
+  const db = req.app.get("db");
+  const language = req.language.id;
+  const guess = req.body.guess;
+
+  if (!guess) {
+    res.status(400).json({ error: "Missing 'guess' in request body" });
+  }
+  //request user words
+  const words = await LanguageService.getLanguageWords(db, language);
+  //find the head
+  const [{ head }] = await LanguageService.getHead(db, language);
+  const list = LanguageService.generateLinkedList(words, head);
+  const [checkGuess] = await LanguageService.checkGuess(db, language);
+
+  if (checkGuess.translation === guess) {
+    //if guess is correct, double the memory value and add a correct counter value
+    const mv = list.head.value.memory_value * 2;
+    list.head.value.memory_value = mv;
+    list.head.value.correct_count++;
+
+    //send the correctly guessed word to the back of list
+    let curr = list.head;
+    let count = mv;
+    while (count > 0 && curr.next !== null) {
+      curr = curr.next;
+      count--;
+    }
+  } else {
+    list.head.value.memory_value = 1;
+    list.head.value.incorrect_count++;
+
+    let curr = list.head;
+    let count = 1;
+    while (count > 0) {
+      curr = curr.next;
+      count--;
+    }
+  }
+
+  res.json({
+    nextWord: list.head.value.original,
+    totalScore: req.lagnauge.total_score,
+    wordCorrectCount: list.head.value.correct_count,
+    wordIncorrectCount: list.head.value.incorrect_count,
+  });
 });
 
 module.exports = languageRouter;
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,3 +1,5 @@
+const { LinkedList, _Node } = require("..//linkedlist/linkedlist");
+
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
@@ -34,6 +36,47 @@ const LanguageService = {
       .join("language", "word.id", "=", "language.head")
       .select("original", "language_id", "correct_count", "incorrect_count")
       .where({ language_id });
+  },
+  checkGuess(db, language_id) {
+    return db
+      .from("word")
+      .join("language", "word.id", "=", "language.head")
+      .select("*")
+      .where({ language_id });
+  },
+
+  getHead(db, language_id) {
+    return db
+      .from("language")
+      .join("word", "word.language_id", "=", "language.id")
+      .select("head")
+      .where({ language_id });
+  },
+  generateLinkedList(words, head) {
+    const currentHead = words.find((word) => word.id === head);
+    const headNodeIdx = words.indexOf(currentHead);
+    const headNode = words.splice(headNodeIdx, 1);
+    const list = new LinkedList();
+
+    list.insertLast(headNode[0]);
+
+    let nextIdx = headNode[0].next;
+    let currentWord = headNode.value;
+    //list.insertLast(currentWord);
+    // nextIdx = currentWord.next;
+    // currentWord = words.find((word) => word.id === nextIdx);
+
+    //generates list via loop given current db
+    while (currentWord !== null) {
+      list.insertLast(currentWord);
+      nextIdx = currentWord.next;
+      if (nextIdx === null) {
+        currentWord = null;
+      } else {
+        currentWord = words.find((word) => word.id === nextIdx);
+      }
+      return list;
+    }
   },
 };
 
